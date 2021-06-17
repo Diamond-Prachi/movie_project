7410fbeff6114c27f65f584c7e1146804cef091a
dlmeredi <dlmeredi@gmail.com>
2021-06-17 14:11:00 -0500

    adds edit functionality

--- js/REST_util.js
+++ js/REST_util.js
@@ -1,7 +1,8 @@
 "use strict";
 getMovies();
 //postMovie();
-deleteMovie();
+// deleteMovie();
+updateMovie(19);
 
 function getMovies() {
     const url = `https://movie-project-diamond-prachi.glitch.me/movies`;
@@ -52,4 +53,30 @@ function deleteMovie(id) {
     fetch(url, options)
         .then(response => console.log(response.json())) /* Movie was created successfully */
         .catch(error => console.error(error)); /* handle errors */
+}
+
+function updateMovie(id) {
+    let movie = {
+        "title": "Harry Potter 10",
+        "rating": "5",
+        "year": "2004",
+        "genre": "fantasy, adventure, narrative",
+        "director": "Alfonso Cuarón",
+        "plot": "Harry Potter's (Daniel Radcliffe) third year at Hogwarts starts off badly when he learns deranged killer Sirius Black",
+        "actors": "DANIEL RADCLIFFE, EMMA WATSON, RUPERT GRINT, MAGGIE SMITH, ALAN RICKMAN, GARY OLDMAN, FIONA SHAW, TIMOTHY SPALL, EMMA THOMPSON"
+    }
+
+    const url = `https://movie-project-diamond-prachi.glitch.me/movies/${id}`;
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(movie)
+
+    };
+    fetch(url, options)
+        .then(response => console.log(response.json())) /* Movie was created successfully */
+        .catch(error => console.error(error)); /* handle errors */
+
 }
